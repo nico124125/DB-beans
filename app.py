26a4df9bf28a8f40b4b4b7632b6e3ba9e4527600
nico124125 <nicoc12@kprschools.ca>
2026-04-01 14:17:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import database
 
 
+MENU_PROMPT = """--- Coffee Bean App ---
+
+Please choose one of these options:
+
+1) Add a new bean.
+2) See all beans.
+3) Find a bean by name.
+4) See which preparation method is best for a beans. 
+5) Exit.
+
+Your selection:"""
+
+
 def menu():
     connection = database.connect()
     database.create.tables(connection)
+
+    while (user_input := input(MENU_PROMPT)) != "5":
+        print(user_input)
 
