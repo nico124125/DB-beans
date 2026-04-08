40851362278c820e35933279660645e780e0334c
nico124125 <nicoc12@kprschools.ca>
2026-04-08 12:52:36 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 import sqlite3
 
 
-CREATE_BEANS_TABLE = "CREATE TABLE IF NOT EXISTS beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);"
+CREATE_PLANE_TABLE = "CREATE TABLE IF NOT EXISTS planes (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);"
 
-INSERT_BEAN = "INSERT INTO beans (name, method, rating) VALUES (?, ?, ?);"
-DELETE_BEAN_BY_NAME = "DELETE FROM beans WHERE name = ?;"
-GET_ALL_BEANS = "SELECT * FROM beans;"
-GET_BEANS_BY_NAME = "SELECT * FROM beans WHERE name = ?;"
-GET_BEST_PREPARATION_FOR_BEAN = """
-SELECT * FROM beans
+INSERT_PLANE = "INSERT INTO planes (name, method, rating) VALUES (?, ?, ?);"
+DELETE_PLANE_BY_NAME = "DELETE FROM planes WHERE name = ?;"
+GET_ALL_PLANES = "SELECT * FROM planes;"
+GET_PLANES_BY_NAME = "SELECT * FROM planes WHERE name = ?;"
+DELETE_PLANE_BY_ID = "DELETE FROM plane WHERE id"
+GET_BEST_PREPARATION_FOR_PLANE = """
+SELECT * FROM planes
 WHERE name = ?
 ORDER BY rating DESC
 LIMIT 1;"""
@@ -20,29 +21,32 @@ def connect():
 
 def create_tables(connection):
     with connection:
-        connection.execute(CREATE_BEANS_TABLE)
+        connection.execute(CREATE_PLANE_TABLE)
 
 
-def add_bean(connection, name, method, rating):
+def add_plane(connection, name, method, rating):
     with connection:
-        connection.execute(INSERT_BEAN, (name, method, rating))
+        connection.execute(INSERT_PLANE, (name, method, rating))
 
 
 
-def get_all_beans(connection):
+def get_all_planes(connection):
     with connection:
-        return connection.execute(GET_ALL_BEANS).fetchall()
+        return connection.execute(GET_ALL_PLANES).fetchall()
 
 
-def get_beans_by_name(connection, name):
+def get_planes_by_name(connection, name):
     with connection:
-        return connection.execute(GET_BEANS_BY_NAME, (name,)).fetchall()
+        return connection.execute(GET_PLANES_BY_NAME, (name,)).fetchall()
 
-def get_best_preparation_for_bean(connection, name):
+def get_best_preparation_for_plane(connection, name):
     with connection:
-        return connection.execute(GET_BEST_PREPARATION_FOR_BEAN, (name, )).fetchone()
+        return connection.execute(GET_BEST_PREPARATION_FOR_PLANE, (name, )).fetchone()
 
-def delete_bean_by_name(connection, name):
+def delete_plane_by_name(connection, name):
     with connection:
-        return connection.execute(DELETE_BEAN_BY_NAME, (name, ))
+        return connection.execute(DELETE_PLANE_BY_NAME, (name, ))
 
+def delete_plane_by_id(connection, name):
+    with connection:
+        return connection.execute(DELETE_PLANE_BY_ID, (name, ))
