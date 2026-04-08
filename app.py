5a0090683c76ecc4e029a1b3b7ae69b071b27f25
nico124125 <nicoc12@kprschools.ca>
2026-04-08 12:32:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ Please choose one of these options:
 2) See all beans.
 3) Find a bean by name.
 4) See which preparation method is best for a beans. 
-5) Exit.
+5) delete bean
+6) Exit.
 
-Your selection:"""
+Your selection: """
 
 
 def menu():
     connection = database.connect()
     database.create_tables(connection)
 
-    while (user_input := input(MENU_PROMPT)) != "5":
+    while (user_input := input(MENU_PROMPT)) != "6":
         if user_input == "1":
             prompt_add_new_bean(connection)
         elif user_input == "2":
@@ -27,6 +28,8 @@ def menu():
             prompt_find_bean(connection)
         elif user_input == "4":
             prompt_find_best_method(connection)
+        elif user_input == "5":
+            prompt_delete_bean(connection)
         else:
             print("Invalid input, please try again")
 
@@ -47,14 +50,16 @@ def prompt_find_bean(connection):
     name = input("Enter bean name to find: ")
     bean = database.get_beans_by_name(connection, name)
 
-    for bean in beans:
+    for bean in bean:
         print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
 
 def prompt_find_best_method(connection):
     name = input("Enter bean name to find: ")
     best_method = database.get_best_preparation_for_bean(connection, name)
 
-
+def prompt_delete_bean(connection):
+    name = input("Enter bean name to delete: ")
+    delete = database.delete_bean_by_name(connection, name)
 
 
 
