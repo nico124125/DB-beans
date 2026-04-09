62a0f7b6e3dbfb2b4378bd20c31f0d4beefbf032
nico124125 <nicoc12@kprschools.ca>
2026-04-09 12:41:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ Please choose one of these options:
 3) Find a plane by name.
 4) See which role is best for plane. 
 5) delete plane
-6) Exit.
+6) delete plane by ID
+7) Exit.
 
 Your selection: """
 
@@ -19,7 +20,7 @@ def menu():
     connection = database.connect()
     database.create_tables(connection)
 
-    while (user_input := input(MENU_PROMPT)) != "6":
+    while (user_input := input(MENU_PROMPT)) != "7":
         if user_input == "1":
             prompt_add_new_plane(connection)
         elif user_input == "2":
@@ -30,6 +31,8 @@ def menu():
             prompt_find_best_method(connection)
         elif user_input == "5":
             prompt_delete_plane(connection)
+        elif user_input == "6":
+            prompt_delete_plane_by_id(connection)
         else:
             print("Invalid input, please try again")
 
@@ -44,14 +47,14 @@ def prompt_see_all_planes(connection):
     planes = database.get_all_planes(connection)
 
     for plane in planes:
-        print(f"{plane[1]} ({plane[2]}) - {plane[3]}/100")
+        print(f"ID: {plane[0]} | {plane[1]} ({plane[2]}) - {plane[3]}/100")
 
 def prompt_find_plane(connection):
     name = input("Enter plane name to find: ")
     plane = database.get_planes_by_name(connection, name)
 
     for plane in plane:
-        print(f"{plane[1]} ({plane[2]}) - {plane[3]}/100")
+        print(f"ID: {plane[0]} | {plane[1]} ({plane[2]}) - {plane[3]}/100")
 
 def prompt_find_best_method(connection):
     name = input("Enter plane name to find: ")
@@ -61,71 +64,9 @@ def prompt_delete_plane(connection):
     name = input("Enter plane name to delete: ")
     delete = database.delete_plane_by_name(connection, name)
 
-
-
-
-menu()import database
-
-
-MENU_PROMPT = """--- Coffee Bean App ---
-
-Please choose one of these options:
-
-1) Add a new bean.
-2) See all beans.
-3) Find a bean by name.
-4) See which preparation method is best for a beans. 
-5) delete bean
-6) Exit.
-
-Your selection: """
-
-
-def menu():
-    connection = database.connect()
-    database.create_tables(connection)
-
-    while (user_input := input(MENU_PROMPT)) != "6":
-        if user_input == "1":
-            prompt_add_new_bean(connection)
-        elif user_input == "2":
-            prompt_see_all_beans(connection)
-        elif user_input == "3":
-            prompt_find_bean(connection)
-        elif user_input == "4":
-            prompt_find_best_method(connection)
-        elif user_input == "5":
-            prompt_delete_bean(connection)
-        else:
-            print("Invalid input, please try again")
-
-def prompt_add_new_bean(connection):
-    name = input("Enter bean name: ")
-    method = input("Enter how you've prepared it: ")
-    rating = int(input("Enter your rating score (0-100): "))
-
-    database.add_bean(connection, name, method, rating)
-
-def prompt_see_all_beans(connection):
-    beans = database.get_all_beans(connection)
-
-    for bean in beans:
-        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
-
-def prompt_find_bean(connection):
-    name = input("Enter bean name to find: ")
-    bean = database.get_beans_by_name(connection, name)
-
-    for bean in bean:
-        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
-
-def prompt_find_best_method(connection):
-    name = input("Enter bean name to find: ")
-    best_method = database.get_best_preparation_for_bean(connection, name)
-
-def prompt_delete_bean(connection):
-    name = input("Enter bean name to delete: ")
-    delete = database.delete_bean_by_name(connection, name)
+def prompt_delete_plane_by_id(connection):
+    plane_id = input("Enter plane ID to delete: ")
+    delete = database.delete_plane_by_id(connection, plane_id)
 
 
 
